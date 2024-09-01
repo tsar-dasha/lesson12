12a8390d9c092fef6c9032798f273cae4a5fdb77
Tsareva Darya <d.tsareva@space307.com>
2024-09-01 17:54:06 +0200

    delete url

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,10 +3,12 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
-    void systemPropertiesTests(){
+    void systemPropertiesTests() {
         System.getProperty("browser", "chrome");
         String browser = System.getProperty("browser");
         System.out.println(browser);
@@ -14,9 +16,17 @@ public class SystemPropertiesTests {
 
     @Test
     @Tag("property")
-    void systemProperties2Tests(){
+    void systemProperties2Tests() {
         System.getProperty("browser", "chrome");
         String browser = System.getProperty("browser");
         System.out.println(browser);
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+        System.out.println(message);
     }
 }
