1060d268818e12191c9a18e396add3d39c065f3f
Tsareva Darya <d.tsareva@space307.com>
2024-09-01 18:47:59 +0200

    added base url

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,7 +12,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-//        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
