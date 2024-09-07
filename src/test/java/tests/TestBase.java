b1df64ab2dbe924e9ca4d959936ad5d8ba3f8ced
Tsareva Darya <d.tsareva@space307.com>
2024-09-07 19:56:45 +0200

    added fixes after review

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,9 +1,13 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -22,9 +26,20 @@ public class TestBase {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
 
+    @BeforeEach
+    public void initEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-//        Configuration.holdBrowserOpen = true;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
 
